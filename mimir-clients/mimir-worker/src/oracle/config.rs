cdd6c8f7ea3fcd42640cfa2c0832827452daf2dc
Forrest Marshall <forrest@mimirblockchain.solutions>
2018-05-15 15:16:50 -0400

    update config values

--- mimir-clients/mimir-worker/src/oracle/config.rs
+++ mimir-clients/mimir-worker/src/oracle/config.rs
@@ -21,10 +21,6 @@ pub struct Config {
     #[serde(rename = "bridge-portal", default = "defaults::bridge_portal", with = "url_serde")]
     pub bridge_portal: Url,
 
-    /// address of the worker-login api portal
-    #[serde(rename = "login-portal", default = "defaults::login_portal", with = "url_serde")]
-    pub login_portal: Url,
-   
     /// address of the auto-funding api portal
     #[serde(rename = "fund-portal", default = "defaults::fund_portal", with = "url_serde")]
     pub fund_portal: Url,
@@ -56,7 +52,6 @@ impl Default for Config {
             mimir_contract: defaults::mimir_contract(),
             websocket_rpc: defaults::websocket_rpc(),
             bridge_portal: defaults::bridge_portal(),
-            login_portal: defaults::login_portal(),
             fund_portal: defaults::fund_portal(),
         }
     }
@@ -67,17 +62,14 @@ impl Default for Config {
 //
 mod defaults {
     use mimir_types::Address;
-    use url::Url;
-   
-    // 0x6b75a9D62C8C3Bf9A8febED2E937f7aa10AeBb86
-    const MIMIR: [u8;20] = [ 
-        0x6b, 0x75, 0xa9, 0xD6, 0x2C, 0x8C, 0x3B, 0xf9, 0xA8, 0xfe,
-        0xbE, 0xD2, 0xE9, 0x37, 0xf7, 0xaa, 0x10, 0xAe, 0xBb, 0x86
-    ];
-    
+    use url::Url; 
+
     const HOST: &'static str = "b2i.io";
 
-    pub fn mimir_contract() -> Address { Address::from(MIMIR) }
+    pub fn mimir_contract() -> Address {
+        "0xc0D9ac1dA08c9744DF9aa09699cC5bf7DFc29AC6".parse()
+            .expect("default mimir address must parse")
+    }
 
     pub fn websocket_rpc() -> Url {
         "ws://127.0.0.1:8546".parse().expect("default rpc address must parse")
@@ -86,11 +78,6 @@ mod defaults {
     pub fn bridge_portal() -> Url {
         //let url_string = format!("wss://{}:8080/mimir-bridge",HOST);
         let url_string = format!("wss://{}:8080/",HOST);
-        Url::parse(&url_string).expect("default always valid")
-    }
-
-    pub fn login_portal() -> Url { 
-        let url_string = format!("https://{}/worker-login",HOST);
         Url::parse(&url_string).expect("default always valid")
     }
 
