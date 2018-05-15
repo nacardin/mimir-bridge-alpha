b2a1615b7f21ca1e7d4498887035c797d986f40c
Forrest Marshall <forrest@mimirblockchain.solutions>
2018-05-15 13:11:52 -0400

    skip seeding for empty sets

--- mimir-clients/mimir-service/src/auth_seeder/mod.rs
+++ mimir-clients/mimir-service/src/auth_seeder/mod.rs
@@ -14,7 +14,8 @@ pub use self::options::Options;
 pub use self::config::Config;
 
 
-use futures::future::Future;
+use futures::future::{self,Either};
+use futures::Future;
 use mimir_transport::redis::{
     RedisNonBlock,
     Error,
@@ -27,13 +28,21 @@ use mimir_transport::redis::{
 // and take care to use proper ordering.
 //
 pub fn apply_seeding<R: RedisNonBlock>(redis: R, seed_state: SeedState) -> impl Future<Item=(),Error=Error> {
-    let SeedState { role, conn, auth } = seed_state;
-    let conn_idents = conn.iter().map(|addr| format!("{}::{:#}",role,addr));
-    let auth_idents = auth.iter().map(|addr| format!("{}::{:#}",role,addr));
-    let conn_key = format!("{}::conn-lease",role);
-    let auth_key = format!("{}::auth-lease",role);
-    let conn_work = redis.sadd(conn_key,conn_idents);
-    let auth_work = redis.sadd(auth_key,auth_idents);
+    let SeedState { role, conn, auth } = seed_state;   
+    let conn_work = if conn.len() > 0 {
+        let conn_idents = conn.iter().map(|addr| format!("{}::{:#}",role,addr));
+        let conn_key = format!("{}::conn-lease",role);
+        Either::A(redis.sadd(conn_key,conn_idents))
+    } else {
+        Either::B(future::ok(0))
+    };
+    let auth_work = if auth.len() > 0 {
+        let auth_idents = auth.iter().map(|addr| format!("{}::{:#}",role,addr));
+        let auth_key = format!("{}::auth-lease",role);
+        Either::A(redis.sadd(auth_key,auth_idents))
+    } else {
+        Either::B(future::ok(0))
+    };
     let seed_work = conn_work.join(auth_work).map(|(c,a)| {
         debug!("seeded {} to `conn` and {} to `auth`...",c,a)
     });
