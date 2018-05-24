7ff8dd46afa3680bc92f0b8ee0739b347d6c971d
Forrest Marshall <forrest@mimirblockchain.solutions>
2018-05-24 14:54:33 -0400

    add basic timeouts to faucet server

--- mimir-clients/mimir-worker/src/bin/faucet-server.rs
+++ mimir-clients/mimir-worker/src/bin/faucet-server.rs
@@ -1,6 +1,7 @@
 extern crate mimir_transport;
 extern crate mimir_worker;
 extern crate mimir_node;
+extern crate tokio_timer;
 extern crate tokio_core;
 extern crate futures;
 extern crate env_logger;
@@ -8,6 +9,8 @@ extern crate env_logger;
 extern crate log;
 
 
+use tokio_timer::Deadline;
+use std::time::{Duration,Instant};
 use mimir_transport::edge;
 use mimir_worker::{common,faucet};
 use tokio_core::reactor::Core;
@@ -47,19 +50,25 @@ fn main() {
 
     let ws_server = ws::server::bind(&handle,opt.serve_address).unwrap();
 
-    let work = ws_server.map_err(|error| error.into())
+    let max_handshake = Duration::from_millis(1024);
+
+    let max_fund_time = Duration::from_secs(64);
+
+    let work = ws_server.map_err(|e| error!("in server stream {:?}",e))
         .and_then(|(client,address)| {
             info!("incoming connection from {}",address);
-            edge::init_server_side_client(client)
+            Deadline::new(
+                edge::init_server_side_client(client),
+                Instant::now() + max_handshake
+                ).map_err(|e| error!("in handshake {:?}",e))
         })
         .and_then(move |(ident,tx,rx)| {
-            if let Some(fund_work) = faucet.get_fund_work(ident.address) {
+            if let Some(base_fund_work) = faucet.get_fund_work(ident.address) {
                 info!("accepting fund request for {}",ident);
-                let fund_work = fund_work
-                    .map_err(|err| {
-                        error!("during funding work {:?}",err);
-                        "web3 error during funding attempt".into()
-                    })
+                let fund_work = Deadline::new(
+                    base_fund_work,
+                    Instant::now() + max_fund_time
+                    ).map_err(|e| error!("in fund work work {:?}",e))
                     .and_then(move |tx_hash| {
                         info!("{} funded with tx {}",ident,tx_hash);
                         let _ = (tx,rx); // TODO: return success response
