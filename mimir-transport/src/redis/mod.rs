05bc1beb29dbafacdadcac2888932c45fe095763
Nick Cardin <nick@cardin.email>
2018-06-08 12:35:42 -0400

    copy old redis submodule

---
+++ mimir-transport/src/redis/mod.rs
@@ -0,0 +1,77 @@
+//! fundamental building blocks for redis based producer/consumer models.
+//!
+//! This module exposes separate "blocking" and "non-blocking" redis handles
+//! for the blocking and non-blocking subsets of redis commands (both types of
+//! handle are asynchronous rust code).
+//!
+//! ```
+//! extern crate mimir_transport;
+//! extern crate tokio_core;
+//! 
+//! use mimir_transport::redis::spawn_nonblock;
+//! use tokio_core::reactor::Core;
+//! # fn example() {
+//!
+//! let mut core = Core::new().unwrap();
+//!
+//! let handle = core.handle();
+//!
+//! let address = "127.0.0.1:6379".parse().unwrap();
+//! 
+//! // create new redis connection
+//! let redis = core.run(spawn_nonblock(&address,handle))
+//!     .expect("unable to connect to redis instance");
+//! 
+//! // execute non-blocking right-pop from list
+//! match core.run(redis.rpop("some-list")).unwrap() {
+//!     Some(item) => println!("popped item from list: {}",item),
+//!     None => println!("list is currently empty"),
+//! }
+//!
+//! # }
+//! # fn main() { }
+//! ``` 
+//!
+
+pub(crate) mod commands;
+mod pop_stream;
+mod push_sink;
+mod blocking;
+mod nonblock;
+
+
+pub use self::blocking::{
+    RedisBlocking,
+    BlockingHandle,
+    Blocking,
+};
+pub use self::nonblock::{
+    RedisNonBlock,
+    NonBlockHandle,
+    NonBlock,
+};
+
+pub use self::pop_stream::PopStream;
+pub use self::push_sink::PushSink;
+
+pub use redis_async::client::paired::SendBox;
+pub use redis_async::error::Error;
+
+use futures::future::{Future,Executor};
+use std::net::SocketAddr;
+
+
+/// spawn new blocking redis handle
+///
+pub fn spawn_blocking<E>(address: &SocketAddr, executor: E) -> Box<Future<Item=BlockingHandle,Error=Error>>
+        where E: Executor<Box<Future<Item=(),Error=()> + Send>> + 'static {
+    BlockingHandle::new(address,executor)
+}
+
+
+/// spanw new nonblocking redis handle
+///
+pub fn spawn_nonblock<E>(address: &SocketAddr, executor: E) -> Box<Future<Item=NonBlockHandle,Error=Error>>
+        where E: Executor<Box<Future<Item=(),Error=()> + Send>> + 'static {
+    NonBlockHandle::new(address,executor)
+}
---
+++ mimir-transport/src/redis/nonblock.rs
@@ -0,0 +1,212 @@
+use futures::future::{Future,Executor};
+use redis_async::client::paired::{
+    PairedConnection,
+    SendBox,
+};
+use redis_async::error::Error;
+use redis_async::client;
+use redis::commands;
+use std::net::SocketAddr;
+use std::sync::Arc;
+use std::rc::Rc;
+
+
+/// wrapper which restricts a redis connection to non-blocking commands only
+///
+#[derive(Clone)]
+pub struct NonBlock<T> {
+    inner: T
+}
+
+
+/// shareable redis handle for execution of non-blocking redis commands
+pub type NonBlockHandle = NonBlock<Arc<PairedConnection>>;
+
+
+impl NonBlock<Arc<PairedConnection>> {
+
+    /// instantiate new nonblocking handle instance
+    pub fn new<E>(addr: &SocketAddr, executor: E) -> Box<Future<Item=Self,Error=Error>> 
+            where E: Executor<Box<Future<Item=(),Error=()> + Send>> + 'static {
+        let work = client::paired_connect(addr,executor)
+            .map(|conn| {
+                let inner = Arc::new(conn);
+                Self { inner }
+            });
+        Box::new(work)
+    }
+}
+
+
+impl<T> NonBlock<T> where T: RedisNonBlock {
+    
+    /// SADD -- add member(s) to set
+    pub fn sadd<K: Into<String>, M: IntoIterator<Item=String>>(&self, key: K, members: M) -> SendBox<i64> { self.inner.sadd(key,members) }
+
+    /// SREM -- remove matching member(s) from set
+    pub fn srem<K: Into<String>, M: IntoIterator<Item=String>>(&self, key: K, members: M) -> SendBox<i64> { self.inner.srem(key,members) }
+
+    /// SMOVE -- move matching member from one set to another
+    pub fn smove<K: Into<String>, M: Into<String>>(&self, source: K, dest: K, member: M) -> SendBox<i64> { self.inner.smove(source,dest,member) }
+
+    /// RPOP -- non-blocking right-handed pop
+    pub fn rpop<K: Into<String>>(&self, key: K) -> SendBox<Option<String>> { self.inner.rpop(key) }
+
+    /// LPUSH -- non-blocking left-handed push
+    pub fn lpush<K: Into<String>, V: IntoIterator<Item=String>>(&self, key: K, values: V) -> SendBox<i64> { self.inner.lpush(key,values) }
+}
+
+
+impl<T> RedisNonBlock for NonBlock<T> where T: RedisNonBlock {
+  
+    fn sadd<K: Into<String>, M: IntoIterator<Item=String>>(&self, key: K, members: M) -> SendBox<i64> { self.sadd(key,members) }
+
+    fn srem<K: Into<String>, M: IntoIterator<Item=String>>(&self, key: K, members: M) -> SendBox<i64> { self.srem(key,members) }
+
+    fn smove<K: Into<String>, M: Into<String>>(&self, source: K, dest: K, member: M) -> SendBox<i64> { self.smove(source,dest,member) }
+    
+    fn rpop<K: Into<String>>(&self, key: K) -> SendBox<Option<String>> { self.rpop(key) }
+
+    fn lpush<K: Into<String>, V: IntoIterator<Item=String>>(&self, key: K, values: V) -> SendBox<i64> { self.lpush(key,values) }
+
+}
+
+
+/// nonblocking redis commands
+pub trait RedisNonBlock {
+
+    /// SADD -- add member(s) to set
+    fn sadd<K: Into<String>, M: IntoIterator<Item=String>>(&self, key: K, members: M) -> SendBox<i64>;
+    
+    /// SREM -- remove matching member(s) from set
+    fn srem<K: Into<String>, M: IntoIterator<Item=String>>(&self, key: K, members: M) -> SendBox<i64>;
+
+    /// SMOVE -- move matching member from one set to another
+    fn smove<K: Into<String>, M: Into<String>>(&self, source: K, dest: K, member: M) -> SendBox<i64>;
+
+    /// RPOP -- non-blocking right-handed pop
+    fn rpop<K: Into<String>>(&self, key: K) -> SendBox<Option<String>>;
+
+    /// LPUSH -- non-blocking left-handed push
+    fn lpush<K: Into<String>, V: IntoIterator<Item=String>>(&self, key: K, values: V) -> SendBox<i64>;
+}
+
+
+impl RedisNonBlock for PairedConnection {
+
+    fn sadd<K: Into<String>, M: IntoIterator<Item=String>>(&self, key: K, members: M) -> SendBox<i64> {
+        self.send(commands::sadd(key,members))
+    }
+
+    fn srem<K: Into<String>, M: IntoIterator<Item=String>>(&self, key: K, members: M) -> SendBox<i64> {
+        self.send(commands::srem(key,members))
+    }
+
+    fn smove<K: Into<String>, M: Into<String>>(&self, source: K, dest: K, member: M) -> SendBox<i64> {
+        self.send(commands::smove(source,dest,member))
+    }
+
+    fn rpop<K: Into<String>>(&self, key: K) -> SendBox<Option<String>> {
+        self.send(commands::rpop(key))
+    }
+
+    fn lpush<K: Into<String>, V: IntoIterator<Item=String>>(&self, key: K, values: V) -> SendBox<i64> {
+        self.send(commands::lpush(key,values))
+    }
+}
+
+
+impl<'a,T> RedisNonBlock for &'a T where T: RedisNonBlock {
+
+    fn sadd<K: Into<String>, M: IntoIterator<Item=String>>(&self, key: K, members: M) -> SendBox<i64> {
+        <T as RedisNonBlock>::sadd(self,key,members)
+    }
+
+    fn srem<K: Into<String>, M: IntoIterator<Item=String>>(&self, key: K, members: M) -> SendBox<i64> {
+        <T as RedisNonBlock>::srem(self,key,members)
+    }
+
+    fn smove<K: Into<String>, M: Into<String>>(&self, source: K, dest: K, member: M) -> SendBox<i64> {
+        <T as RedisNonBlock>::smove(self,source,dest,member)
+    }
+
+    fn rpop<K: Into<String>>(&self, key: K) -> SendBox<Option<String>> {
+        <T as RedisNonBlock>::rpop(self,key)
+    }
+
+    fn lpush<K: Into<String>, V: IntoIterator<Item=String>>(&self, key: K, values: V) -> SendBox<i64> {
+        <T as RedisNonBlock>::lpush(self,key,values)
+    }
+}
+
+
+impl<T> RedisNonBlock for Arc<T> where T: RedisNonBlock {
+
+    fn sadd<K: Into<String>, M: IntoIterator<Item=String>>(&self, key: K, members: M) -> SendBox<i64> {
+        <T as RedisNonBlock>::sadd(self,key,members)
+    }
+
+    fn srem<K: Into<String>, M: IntoIterator<Item=String>>(&self, key: K, members: M) -> SendBox<i64> {
+        <T as RedisNonBlock>::srem(self,key,members)
+    }
+
+    fn smove<K: Into<String>, M: Into<String>>(&self, source: K, dest: K, member: M) -> SendBox<i64> {
+        <T as RedisNonBlock>::smove(self,source,dest,member)
+    }
+
+    fn rpop<K: Into<String>>(&self, key: K) -> SendBox<Option<String>> {
+        <T as RedisNonBlock>::rpop(self,key)
+    }
+
+    fn lpush<K: Into<String>, V: IntoIterator<Item=String>>(&self, key: K, values: V) -> SendBox<i64> {
+        <T as RedisNonBlock>::lpush(self,key,values)
+    }
+}
+
+
+impl<T> RedisNonBlock for Rc<T> where T: RedisNonBlock {
+
+    fn sadd<K: Into<String>, M: IntoIterator<Item=String>>(&self, key: K, members: M) -> SendBox<i64> {
+        <T as RedisNonBlock>::sadd(self,key,members)
+    }
+
+    fn srem<K: Into<String>, M: IntoIterator<Item=String>>(&self, key: K, members: M) -> SendBox<i64> {
+        <T as RedisNonBlock>::srem(self,key,members)
+    }
+
+    fn smove<K: Into<String>, M: Into<String>>(&self, source: K, dest: K, member: M) -> SendBox<i64> {
+        <T as RedisNonBlock>::smove(self,source,dest,member)
+    }
+
+    fn rpop<K: Into<String>>(&self, key: K) -> SendBox<Option<String>> {
+        <T as RedisNonBlock>::rpop(self,key)
+    }
+
+    fn lpush<K: Into<String>, V: IntoIterator<Item=String>>(&self, key: K, values: V) -> SendBox<i64> {
+        <T as RedisNonBlock>::lpush(self,key,values)
+    }
+}
+
+
+impl<T> RedisNonBlock for Box<T> where T: RedisNonBlock {
+
+    fn sadd<K: Into<String>, M: IntoIterator<Item=String>>(&self, key: K, members: M) -> SendBox<i64> {
+        <T as RedisNonBlock>::sadd(self,key,members)
+    }
+
+    fn srem<K: Into<String>, M: IntoIterator<Item=String>>(&self, key: K, members: M) -> SendBox<i64> {
+        <T as RedisNonBlock>::srem(self,key,members)
+    }
+
+    fn smove<K: Into<String>, M: Into<String>>(&self, source: K, dest: K, member: M) -> SendBox<i64> {
+        <T as RedisNonBlock>::smove(self,source,dest,member)
+    }
+
+    fn rpop<K: Into<String>>(&self, key: K) -> SendBox<Option<String>> {
+        <T as RedisNonBlock>::rpop(self,key)
+    }
+
+    fn lpush<K: Into<String>, V: IntoIterator<Item=String>>(&self, key: K, values: V) -> SendBox<i64> {
+        <T as RedisNonBlock>::lpush(self,key,values)
+    }
+}
