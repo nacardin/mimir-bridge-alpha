274429b2f426b8ce5ea9233404e555b82ea3cb0b
Forrest Marshall <forrest@mimirblockchain.solutions>
2018-06-11 15:10:19 -0400

    reexport subcrates

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,9 @@
-//! Experimental Rust implementation of Mimir Bridge clients & protocols.
-//! 
-//! 
-//! - [`mimir-proto`](../mimir_proto/index.html): core protocol implementations & helpers.
+//! golden gate got nothing on this
 //!
-//! - [`mimir-crypto`](../mimir_crypto/index.html): primitives and traits for common cryptographic
-//! operations.
-//!
-//! - [`mimir-types`](../mimir_types/index.html): definitions and reexports of commonly used types.
-//!
-//! - [`mimir-util`](../mimir_util/index.html): various helper functions and macros.
-//!
-//! - [`mimir-worker`](../mimir_worker/index.html): core worker client logic.
-//! 
-//! - [`mimir-transport`](../mimir_transport/index.html): inter-client communication protocol
-//! helpers.
-//! 
-//! - [`mimir-requester`](../mimir_requester/index.html): core requester client logic.
-//! 
-//! - [`mimir-node`](../mimir_node/index.html): utilities for managing and querying local
-//! blockchain nodes.
-//!
+
+pub extern crate mimir_common;
+pub extern crate mimir_crypto;
+pub extern crate mimir_proto;
+pub extern crate mimir_transport;
+pub extern crate mimir_worker;
 
